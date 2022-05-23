442402f5eb85220ba93b24a086fad8d1ac9364b6
Vsevolod Shmelkin <schmel924@gmail.com>
2022-05-23 13:07:42 +0300

    First step

---
+++ main.lua
@@ -0,0 +1,45 @@
+--sheepolution preface
+local launch_type = arg[2]
+if launch_type == "test" or launch_type == "debug" then
+    require "lldebugger"
+    if launch_type == "debug" then
+---@diagnostic disable-next-line: undefined-global
+        lldebugger.start()
+    end
+end
+
+
+
+--Actual code
+function love.load()
+ Rink = love.graphics.newImage("Backstage.png", nil)
+ RinkX, RinkY = Rink:getDimensions()
+ love.window.setMode(RinkX,RinkY,{resizable=true, vsync=false})
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+    love.graphics.setColor (1,1,1)
+    love.graphics.draw (Rink)
+    love.graphics.setColor (0,0,0)
+    love.graphics.rectangle( "fill", RinkX/2, RinkY/2, 50, 50)
+end
+
+
+
+--sheepolution footer
+---@diagnostic disable-next-line: undefined-field
+local love_errorhandler = love.errhand
+function love.errorhandler(msg)
+---@diagnostic disable-next-line: undefined-global
+    if lldebugger then
+---@diagnostic disable-next-line: undefined-global
+        lldebugger.start() -- Add this
+        error(msg, 2)
+    else
+        return love_errorhandler(msg)
+    end
+end
