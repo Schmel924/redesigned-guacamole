913005c8570951079749f3ca47a750f5fc11948f
Vsevolod Shmelkin <schmel924@gmail.com>
2022-05-27 08:38:24 +0300

    LEt's have some direction vector displayed

--- main.lua
+++ main.lua
@@ -14,19 +14,28 @@ Puck = {}
 function DrawPuck ()
     love.graphics.setColor (0,0,0)
     love.graphics.circle('fill', Puck.x, Puck.y, Puck.radius)
+    love.graphics.setColor (1,1,1)
+    love.graphics.line (Puck.x, Puck.y, Puck.x+Puck.dirx*50, Puck.y+Puck.diry*50)
+end
+
+function PuckDirect()
+    local len = math.sqrt(Puck.dx^2+Puck.dy^2)
+    Puck.dirx = Puck.dx/len
+    Puck.diry = Puck.dy/len
 end
 
 function  ResetPuck ()
-    Puck = {dx = love.math.random( -500, 500 ), dy = love.math.random( -500, 500 ), x = RinkX/2, y = RinkY/2, radius = 50}
+    Puck = {dx = love.math.random( -500, 500 ), dy = love.math.random( -500, 500 ), x = RinkX/2, y = RinkY/2, radius = 50} 
+    PuckDirect()
 end
 function UpdatePuck (dt)
     local r = Puck.radius/2
     Puck.x = Puck.x+Puck.dx*dt
     Puck.y = Puck.y+Puck.dy*dt
-    if (Puck.x <= r) then Puck.dx = -Puck.dx end
-    if (Puck.y <= r) then Puck.dy = -Puck.dy end
-    if (Puck.x+r >= RinkX) then Puck.dx = -Puck.dx end
-    if (Puck.y+r >= RinkY) then Puck.dy = -Puck.dy end
+    if (Puck.x <= r) then Puck.dx = -Puck.dx PuckDirect() end
+    if (Puck.y <= r) then Puck.dy = -Puck.dy PuckDirect() end
+    if (Puck.x+r >= RinkX) then Puck.dx = -Puck.dx PuckDirect() end
+    if (Puck.y+r >= RinkY) then Puck.dy = -Puck.dy PuckDirect() end
 end
 
 
