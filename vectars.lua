7468734b34ea5ab4b8885cb8f5bc6d80e7666beb
Vsevolod Shmelkin <schmel924@gmail.com>
2022-07-05 12:58:34 +0300

    СОздал библитечку векторов, ща буду рефакторить

---
+++ vectars.lua
@@ -0,0 +1,42 @@
+function mag (v)
+	return math.sqrt(v.x^2 + v.y^2)
+end
+
+function norm (v)
+	local m = mag (v)
+	local z = v
+	if (m ~= 0) then z.x=z.x/m z.y=z.y/m end 
+	return z
+end
+
+function dist (s,f)
+	local v = {}
+	v.x = f.x - s.x
+	v.y = f.y - s.y
+	return mag (v)
+end
+
+function dir (s,f)
+	local v = {}
+	v.x = f.x - s.x
+	v.y = f.y - s.y
+	return norm (v)
+end
+
+function scale (v,s)
+	local z = {}
+	z.x=v.x*s
+	z.y=v.y*s
+	return z
+end
+
+function dot(s,f)
+    return s.x * f.x + s.y * f.y
+end
+
+function path (s,f)
+	local v = {}
+	v.x = f.x-s.x
+	v.y = f.y-s.x
+	return v
+end
