139eb992d253eec26f8cc749037c41fc88741817
Vsevolod Shmelkin <schmel924@gmail.com>
2022-07-05 12:59:24 +0300

    Main отрефакторил

--- main.lua
+++ main.lua
@@ -8,19 +8,20 @@ if launch_type == "test" or launch_type == "debug" then
     end
 end
 
+require "vectars"
 require "daman"
 
-function GetVector (x1,y1,x2,y2)
-local x = x2-x1
-local y = y2-y1
-return x,y
-end
+--[[function GetVector (x1,y1,x2,y2)
+    local x = x2-x1
+    local y = y2-y1
+    return x,y
+end--]] -- куда её использовал?
 
 function PuckInGoal ()
     --{50, RinkY/3, 200, RinkY/3, 200, RinkY*2/3, 50, RinkY*2/3}
- if Puck.x > LeftGate[1] and Puck.x < LeftGate[3] and Puck.y > LeftGate[2] and Puck.y < LeftGate[6]  then Score("Left") end
- if Puck.x > RightGate[1] and Puck.x < RightGate[3] and Puck.y > RightGate[2] and Puck.y < RightGate[6]  then Score("Right") end
-end
+ if Puck.c.x > LeftGate[1]  and Puck.c.x < LeftGate[3] and Puck.c.y > LeftGate[2] and Puck.c.y < LeftGate[6]  then Score("Left") end
+ if Puck.c.x > RightGate[1] and Puck.c.x < RightGate[3] and Puck.c.y > RightGate[2] and Puck.c.y < RightGate[6]  then Score("Right") end
+end -- Puck.x вот за этим придется вернуться
 
 function Score(s)
     love.graphics.print (s, 20, 10)
@@ -33,55 +34,62 @@ Puck = {}
 function DrawPuck ()
     local r, g, b, a = love.graphics.getColor( )
     love.graphics.setColor (Puck.color)
-    love.graphics.circle('fill', Puck.x, Puck.y, Puck.radius)
+    love.graphics.circle('fill', Puck.c.x, Puck.c.y, Puck.radius) -- и здесь
     love.graphics.setColor (r,g,b,a)
   --[[  love.graphics.setColor (1,1,1)
     love.graphics.line (Puck.x, Puck.y, Puck.x+Puck.dirx*50, Puck.y+Puck.diry*50)
     ]]
 end
 
-function PuckDirect()
+--[[function PuckDirect() -- вот отличный кандидат на замену!
     local len = math.sqrt(Puck.dx^2+Puck.dy^2)
     Puck.dirx = Puck.dx/len
     Puck.diry = Puck.dy/len
+end--]]
+
+function  ResetPuck () --приехали
+    Puck = {radius = 20 , color = {0,0,0}}
+    local c = {x = RinkX/2, y = RinkY/2}
+    local d = {x = love.math.random( -500, 500 ), y = love.math.random( -500, 500 )}
+    local dir  = norm (d) --  PuckDirect()
+    Puck.c = c
+    Puck.d = d
+    Puck.dir = dir
 end
 
-function  ResetPuck ()
-    Puck = {dx = love.math.random( -500, 500 ), dy = love.math.random( -500, 500 ), x = RinkX/2, y = RinkY/2, radius = 20 , color = {0,0,0}}
-    PuckDirect()
-end
 function UpdatePuck (dt)
     local x = false
     local y = false
-    if (Puck.x+(Puck.dx*dt)-Puck.radius < 0)
+    if (Puck.c.x+(Puck.d.x*dt)-Puck.radius < 0)
     then
-        Puck.x = Puck.radius --because center of circle will be exactly radius away from zero
-        Puck.dx = -Puck.dx PuckDirect()
+        Puck.c.x = Puck.radius --because center of circle will be exactly radius away from zero
+        Puck.d.x = -Puck.d.x 
         x = true
     end
-    if (Puck.y+Puck.dy*dt-Puck.radius < 0)
+    if (Puck.c.y+Puck.d.y*dt-Puck.radius < 0)
     then
-        Puck.y = Puck.radius
-        Puck.dy = -Puck.dy PuckDirect()
+        Puck.c.y = Puck.radius
+        Puck.d.y = -Puck.d.y
         y = true
     end
-    if (Puck.x+Puck.radius+Puck.dx*dt > RinkX)
+    if (Puck.c.x+Puck.radius+Puck.d.x*dt > RinkX)
     then
-        Puck.x = RinkX - Puck.radius
-        Puck.dx = -Puck.dx PuckDirect()
+        Puck.c.x = RinkX - Puck.radius
+        Puck.d.x = -Puck.d.x
         x = true
     end
-    if (Puck.y+Puck.radius+Puck.dy*dt > RinkY)
+    if (Puck.c.y+Puck.radius+Puck.d.y*dt > RinkY)
     then
-        Puck.y = RinkY - Puck.radius
-        Puck.dy = -Puck.dy PuckDirect()
+        Puck.c.y = RinkY - Puck.radius
+        Puck.d.y = -Puck.d.y
         y = true
     end
     --Now the question is should we update position after this? If we do, Puck gets reflected by invisible force inch before the wall
     -- now we update x and y only once in a frame, either touchnig the wall, or moving freely
+    Puck.dir = norm (Puck.d)
     PuckInGoal ()
-    if (not x) then Puck.x = Puck.x+Puck.dx*dt end
-    if (not y) then Puck.y = Puck.y+Puck.dy*dt end
+    if (not x) then Puck.c.x = Puck.c.x+Puck.d.x*dt end
+    if (not y) then Puck.c.y = Puck.c.y+Puck.d.y*dt end
 end
 
 
@@ -100,15 +108,14 @@ end
 
 
 function love.update(dt)
-    UpdatePuck (dt)
-    UpdatePlayers (dt)
-    if love.mouse.isDown(1)
+   if love.mouse.isDown(1)
     then
        local x,y = love.mouse.getPosition()
-        Puck.dx = x - Puck.x
-        Puck.dy = y - Puck.y
-        PuckDirect()
+         local v = {x,y}
+         Puck.d = path(Puck.c.v)
     end
+    UpdatePuck (dt)
+    UpdatePlayers (dt)
 end
 
 function love.draw()
