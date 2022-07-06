fa7822a8baa0cb30926881fa36fa7229d2de52ce
Vsevolod Shmelkin <schmel924@gmail.com>
2022-07-06 14:41:52 +0300

    i am done with state_machine for tonight It works, players do things. I am content

--- daman.lua
+++ daman.lua
@@ -3,12 +3,13 @@ PlayerA.c = {}
 PlayerA.c.x = 1000
 PlayerA.c.y = 150
 PlayerA.size = 50
-PlayerA.speed = 15
+PlayerA.speed = 30
 PlayerA.force = 800
 PlayerA.d = {}
 PlayerA.d.x = 0
 PlayerA.d.y = 0
 PlayerA.state = "hunt"
+PlayerA.time = 0
 PlayerB = {}
 PlayerB.c = {}
 PlayerB.d = {}
@@ -18,8 +19,9 @@ PlayerB.size = 50
 PlayerB.d.x = 0
 PlayerB.d.y = 0
 PlayerB.state = "hunt"
-PlayerB.speed = 15
+PlayerB.speed = 30
 PlayerB.force = 800
+PlayerB.time = 0
 Players = {PlayerA, PlayerB}
 Gamestate = "draw"
 
@@ -36,14 +38,18 @@ for i,p in ipairs(Players) do
     p.c.x = p.c.x + p.d.x*dt
     p.c.y = p.c.y + p.d.y*dt
     end
-    if p.state == "attack" then 
+    if p.state == "attack" and love.timer.getTime() - p.time > 1 then 
+        p.time = 0
         LaunchPuck (p)
         p.state = "wait"
     end
     if p.state == "hunt" then
     p.d = scale(dir (p.c,Puck.c),p.speed)
         if CatchPuck (p) then
-        p.state = "attack" Gamestate = "attack"
+        p.time = love.timer.getTime()    
+        p.state = "attack" 
+        p.d = scale (p.d,0)
+        Gamestate = "attack"
         end
     end
 end
