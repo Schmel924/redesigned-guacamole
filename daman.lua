82c33881f12b5768dde8d1eb25d9a670aa96fc34
Vsevolod Shmelkin <schmel924@gmail.com>
2022-07-07 10:51:12 +0300

    У хоккеистов теперь есть толстые члены. Осталось их разбить на команды

--- daman.lua
+++ daman.lua
@@ -57,7 +57,7 @@ end
 
 function CatchPuck(pl)
     if dist (pl.c,Puck.c) < 24 then
-        Puck.color = {1,0,0}
+       -- Puck.color = {1,0,0}
         Puck.d = scale (Puck.d,0) -- какой я вумный
         Puck.c.x = pl.c.x
         Puck.c.y = pl.c.y
@@ -68,8 +68,21 @@ end
 
 
 function DrawPlayers()
+    local r, g, b, a = love.graphics.getColor( )
 for i,v in ipairs(Players) do
     love.graphics.setColor (1,0,0)
-    love.graphics.rectangle( 'fill', v.c.x, v.c.y, v.size, v.size, 0, 0, 3)
+    love.graphics.rectangle( 'fill', v.c.x-v.size/2, v.c.y-v.size/2, v.size, v.size, 0, 0, 3)
+    love.graphics.setColor (1,1,1,1)
+    love.graphics.line(v.c.x,v.c.y, v.c.x+v.d.x, v.c.y+v.d.y)
+   
+    if v.state == "attack" then
+        local d = dir (v.c,LeftGate.center)
+        love.graphics.setColor (1,0,0)
+        width = love.graphics.getLineWidth()
+        love.graphics.setLineWidth((love.timer.getTime()- v.time)*10 )
+        love.graphics.line (v.c.x,v.c.y, v.c.x+d.x *50,(v.c.y)+d.y*50)
+        love.graphics.setLineWidth( width)
+    end
 end
+ love.graphics.setColor (r,g,b,a)
 end
